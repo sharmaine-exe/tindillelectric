105fe0a1a426e248c53d0f2c926d26761447f3b2
Sharmaine Jesm Santos <sharmainejesmsantos@gmail.com>
2023-03-06 15:43:14 -0700

    updated content-cut-sheets.php

--- template-parts/content-cut-sheets.php
+++ template-parts/content-cut-sheets.php
@@ -17,17 +17,56 @@
 	<?php tindillelectric_post_thumbnail(); ?>
 
 	<div class="entry-content">
-		<?php  ?>
-        <a href="<?php the_permalink();?>">
-            <?php the_field('title'); ?>
-        </a>
 
-        <?php the_field('cut_sheet_file'); ?> 
+		<?php 
+			$file = get_field('cut_sheet_file'); 
+			if($file):
+
+				// Extract variables.
+				$url = $file['url'];
+				$title = $file['title'];
+				$caption = $file['caption'];
+				$icon = $file['icon'];
+
+				// Display image thumbnail when possible.
+				if( $file['type'] == 'image' ) {
+					$icon =  $file['sizes']['thumbnail'];
+				}
+
+				// Begin caption wrap.
+				if( $caption ): ?>
+					<div class="wp-caption">
+				<?php endif; ?>
+			
+				<a href="<?php echo esc_attr($url); ?>" title="<?php echo esc_attr($title); ?>">
+					<img src="<?php echo esc_attr($icon); ?>" />
+				</a>
+			
+				<?php 
+				// End caption wrap.
+				if( $caption ): ?>
+					<p class="wp-caption-text"><?php echo esc_html($caption); ?></p>
+					</div>
+				<?php endif; ?>
+			<?php endif; ?> 
+				<h4 class="">
+					View / Download
+					<a href="<?php echo $file['url']; ?>">
+						<?php echo $file['filename']; ?>
+					</a>
+				</h4>
+
+			
+			
+
 
 	</div><!-- .entry-content -->
 
 	<?php if ( get_edit_post_link() ) : ?>
 		<footer class="entry-footer">
+			<div class="cut-sheet-cat-single">
+				<p>Category: <?php the_category(); ?></p>
+			</div>
 			<?php
 			edit_post_link(
 				sprintf(
@@ -50,15 +89,3 @@
 	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
 
-
-
-<div class="entry-content">
-    <a href="<?php the_permalink();?>">
-        <?php the_title('<h3>','</h3>'); ?>
-    </a>
-
-    <!-- advanced custom fields -->
-    <?php the_field('description'); ?> 
-    
-    <?php the_category(); ?>
-</div>
