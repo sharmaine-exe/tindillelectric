da81870ec49162ab9c4dffcf2f2dd613a06b576d
mjayme1 <mjayme1@nait.ca>
2023-03-31 14:18:41 -0600

    styled banner for single services page

--- template-parts/content-services_details.php
+++ template-parts/content-services_details.php
@@ -8,51 +8,56 @@
  */
 
 ?>
-    <div class="container">
-        <article <?php post_class();?> id="post-<?php the_ID();?>" >
+<main id="primary" class="site-main single-service">
+    <article <?php post_class();?> id="post-<?php the_ID();?>" >
 
-            <header>
-                <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>	
-                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-                <?php the_field('banner_description');?>
-                <button><a class="single-request" href="contact">REQUEST A QUOTE</a></button>
-            </header>
-
-            <div class="entry-content">	
-    
-            <?php 
-                $image = get_field('featured_image');
-                $size = 'large'; // (thumbnail, medium, large, full or custom size)
-                if( $image ) {
-                    echo wp_get_attachment_image( $image, $size );
-                }
-            ?>
-            <?php 
-                $image = get_field('service_image_1');
-                $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
-                if( $image ) {
-                    echo wp_get_attachment_image( $image, $size );
-                }
-            ?>
-            <?php 
-                $image = get_field('service_image_2');
-                $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
-                if( $image ) {
-                    echo wp_get_attachment_image( $image, $size );
-                }
-            ?>
-            <?php 
-                $image = get_field('service_image_3');
-                $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
-                if( $image ) {
-                    echo wp_get_attachment_image( $image, $size );
-                }
-            ?>
-
-
-                <?php the_field('service_description');?>
-                
+        <!-- Banner -->
+        <section class="banner banner-single-service" style="background: linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url(<?php echo get_the_post_thumbnail_url()?>)">
+            <div class="container">
+                <div class="text-center">
+                    <h3 class="heading-secondary"><?php the_title(); ?></h3>
+                    <p><?php the_field('banner_description'); ?></p>
+                    <a href="<?php echo esc_url( home_url( '/contact' ) ); ?>" class="btn btn-content">Request A Quote</a>
+                </div>
             </div>
+        </section>
+        <!-- End of Banner -->
 
-        </article>
-    </div>
+        <div class="entry-content">	
+
+        <?php 
+            $image = get_field('featured_image');
+            $size = 'large'; // (thumbnail, medium, large, full or custom size)
+            if( $image ) {
+                echo wp_get_attachment_image( $image, $size );
+            }
+        ?>
+        <?php 
+            $image = get_field('service_image_1');
+            $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
+            if( $image ) {
+                echo wp_get_attachment_image( $image, $size );
+            }
+        ?>
+        <?php 
+            $image = get_field('service_image_2');
+            $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
+            if( $image ) {
+                echo wp_get_attachment_image( $image, $size );
+            }
+        ?>
+        <?php 
+            $image = get_field('service_image_3');
+            $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
+            if( $image ) {
+                echo wp_get_attachment_image( $image, $size );
+            }
+        ?>
+
+
+            <?php the_field('service_description');?>
+            
+        </div>
+
+    </article>
+</main>
