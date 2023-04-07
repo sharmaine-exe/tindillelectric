6b87a3537474aed550a7cc4ea2e65c3dd9e63128
mjayme1 <mjayme1@nait.ca>
2023-04-06 19:51:43 -0600

    refactor sass and html markup - footer

--- footer.php
+++ footer.php
@@ -14,13 +14,6 @@
 	<footer id="colophon" class="site-footer">
 		<div class="footer-flex">
 			<div class="footer-flex-row">
-<!-- 
-				<div class="site-info">
-					<a href="<?php //echo esc_url( __( 'http://tindill.web.dmitcapstone.ca/', 'tindillelectric' ) ); ?>">
-						<img src="http://tindill.web.dmitcapstone.ca/wp-content/uploads/2023/03/logo-white.png" alt="footer-logo">
-					</a>
-				</div>.site-info -->
-
 				<?php
 					dynamic_sidebar('footer_widgets');
 				?>
